82813a740fde489b2e44675253dc7e0393849f5a
yusupovibratbek-coder <yusupovibratbek18@gmail.com>
2026-05-23 03:50:00 -0700

    yangilandi

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,20 +3,18 @@ export default function Hero() {
         <div>
 
             <div className="relative">
-               <video className="w-full" src="fire.mp4" autoPlay muted loop></video>
-               <div>
-                <h1 className="absolute pl-150 pt-1 text-5xl font-bold text-white top-1/2 ">REVOLUTIONIZING SPACE TECHNOLOGY</h1>
-                <h1 className="absolute pl-150 pt-30 top-1/2 text-white">SpaceX's Starship spacecraft and Super Heavy rocket is a fully reusable transportation system designed to carry both crew and cargo to Earth orbit, the Moon, Mars, and beyond.</h1>
-               </div>
+                <video className="w-full" src="fire.mp4" autoPlay muted loop></video>
+                <div>
+                    <h1 className="absolute pl-150 pt-1 text-5xl font-bold text-white top-1/2 ">REVOLUTIONIZING SPACE TECHNOLOGY</h1>
+                    <h1 className="absolute pl-150 pt-30 top-1/2 text-white">SpaceX's Starship spacecraft and Super Heavy rocket is a fully reusable transportation system designed to carry both crew and cargo to Earth orbit, the Moon, Mars, and beyond.</h1>
+                </div>
             </div>
 
-            <div>
+            <div className="relative">
+                <video className="w-full" src="night.mp4" autoPlay muted loop></video>
                 <div>
-                    <div>
-                        <div>
-                            
-                        </div>
-                    </div>
+                    <h1 className="absolute top-1/2 text-white text-5xl font-bold pl-20 w-150">WORLD'S LEADING LAUNCH SERVICE PROVIDER</h1>
+                    <h1 className="w-180 absolute top-1/2 pl-20 pt-40">SpaceX leads the world in launches with its reliable, reusable rockets and is developing the fully and rapidly reusable rockets necessary to transform humanity's ability to access space into something as routine as air travel.</h1>
                 </div>
             </div>
 
