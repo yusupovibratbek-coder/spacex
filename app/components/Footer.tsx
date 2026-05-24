db26722e8ea3481ad9aed0bbb9dfa6ddea26f340
yusupovibratbek-coder <yusupovibratbek18@gmail.com>
2026-05-24 11:30:54 -0700

    yangilandi

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <div className="bg-gray-950 p-2">
+        <div className="bg-gray-950 p-4">
             <div className=" flex justify-around items-center">
                 <a href="https://x.com/SpaceX"><h1 className="border-white border-1 text-white text-2xl bg-gray-900 w-16 p-4 text-center rounded-4xl font-bold font-serif">X</h1></a>
                 <div className="flex gap-10">
